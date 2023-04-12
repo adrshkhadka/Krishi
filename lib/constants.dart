e876f28ebbb4863d8850fbde4a33c963163ab27e
adrshkhadka <khadka03adrsh@gmail.com>
2023-04-12 22:35:51 +0545

    second commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+//constans
 const kPrimaryColor =Colors.green;
 const kPrimaryLightColor = Color.fromARGB(255, 22, 116, 50);
 const kPrimaryGradientColor = LinearGradient(
